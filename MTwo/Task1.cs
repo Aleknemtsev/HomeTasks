e0436f8f34c46eeb30b41705830f768450340a98
Alexander Nemtsev <aleknemtsev@yandex.ru>
2020-03-30 17:36:10 +0300

    Solved task 1

--- MTwo/Task1.cs
+++ MTwo/Task1.cs
@@ -6,9 +6,16 @@ namespace MTwo
 {
 	class Task1
 	{
-		//public static int InsertNumber(int numberSource, int numberIn, int i, int j)
-		//{
+		public static int InsertNumber(int numberSource, int numberIn, int i, int j)
+		{
+			numberIn = numberIn << (31 - (j - i + 1));
+			numberIn = numberIn >> (31 - j - 1);
+			if (numberIn < 0)
+				numberIn = ~numberIn + 1;
 
-		//}
+			int res = numberIn | numberSource;
+
+			return res;
+		}
 	}
 }
