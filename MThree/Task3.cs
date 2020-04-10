d0c8e928b0db427b0dd77da0dbdaf10bbc09c627
Alexander Nemtsev <aleknemtsev@yandex.ru>
2020-04-10 18:08:04 +0300

    Решена 2 задача

---
+++ MThree/Task3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MThree
+{
+	public class Task3
+	{
+		public static string ToBin(int number)
+		{
+			string numberBin = "";
+
+			do
+			{
+				int remainder = number % 2;
+				numberBin += remainder.ToString();
+				number /= 2;
+			}
+			while (number != 0);
+
+			numberBin = StringReverse(numberBin);
+
+			return numberBin;
+		}
+
+		private static string StringReverse(string number)
+		{
+			char[] array = number.ToArray();
+			Array.Reverse(array);
+
+			return new String(array);
+		}
+
+	}
+}
